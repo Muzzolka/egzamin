49a62450046c28ce7ec60345c593d6b85f8d4f58
muzzolka <m.staniszew@gmail.com>
2017-11-21 00:29:15 +0100

    przechodzenie miedzy stronami

--- egzamin.py
+++ egzamin.py
@@ -4,4 +4,10 @@ browser = webdriver.Firefox()
 
 browser.get("https://bilety.teatr-capitol.pl")
 
-browser.find_element_by_css_selector(".txt")
+browser.find_element_by_css_selector(".next").click()
+
+browser.find_element_by_link_text("Makbet").click()
+
+browser.find_element_by_link_text("Kup bilet onlie").click()
+
+browser.find_element_by_link_text("Kup bilet").click()
