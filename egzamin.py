fa6e5b8a492ad7f26af2aea87322caa561ddf4e1
Muzzolka <m.staniszew@gmail.com>
2017-11-27 19:29:18 +0100

    wybór miejsc + naprawa waitów

--- egzamin.py
+++ egzamin.py
@@ -13,9 +13,24 @@ browser.find_element_by_xpath("//a[@class='next']").click()
 
 browser.find_element_by_link_text("Kup bilet").click()
 
-#wait = WebDriverWait(browser, 10)
-#wait.until(browser, EC.presence_of_element_located((By.ID, "5800_l")))
+wait = WebDriverWait(browser, 10)
+wait.until(EC.presence_of_element_located((By.ID, "5519_l"))).click()
+wait.until_not(EC.visibility_of_element_located((By.ID, "loading")))
 
-browser.find_element_by_css_selector("#5801_l").click()
-browser.find_element_by_xpath("//text[@id_miejsca='5800']").click()
-browser.find_element_by_id("5802_1").click()
+# browser.find_element_by_css_selector("#5813_l").click()
+# browser.find_element_by_xpath("//text[@id='5525_l']").click()
+# browser.find_element_by_id("5844_l").click()
+
+wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Dalej >>"))).click()
+
+#browser.find_element_by_css_selector("a.btn.btn-default.btn-nastepny.f-r").click()
+#browser.find_element_by_class_name("btn.btn-default.btn-nastepny.f-r").click()
+
+# wait = WebDriverWait(browser, 20)
+wait.until(EC.title_is("Logowanie - Teatr Muzyczny Capitol"))
+
+browser.find_element_by_id("form_login_login").send_keys("admin@admin")
+
+browser.find_element_by_id("form_login_haslo").send_keys("admin")
+
+browser.find_element_by_id("form_login_submit").click()
---
+++ steps.py
@@ -0,0 +1,6 @@
+Given I am on Capitol Page
+When I click 'Next' button
+And I choose 'Makbet' play to watch
+And I click 'Kup bilet' button to buy tickets
+And I choose '5852' spot
+When I click 'Dalej' button
